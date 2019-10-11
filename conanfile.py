e4e685f345667b10228daece91eaeecc1cd25fa3
Kai Wolf <mail@kai-wolf.me>
2019-10-11 09:27:32 +0200

    Add Conan configuration file
    This commit adds a configuration file for the Conan C/C++ package
    manager [1]. This enables this library to be consumed by other projects
    that use Conan for managing their dependencies.
    
    [1] https://conan.io/

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conans import ConanFile, tools
+
+class TclapConan(ConanFile):
+    name = "tclap"
+    version = "1.2.2"
+    license = "MIT License"
+    author = "Michael E. Smoot"
+    url = "http://github.com/xguerin/tclap"
+    description = "Templatized Command Line Argument Parser"
+    topics = ("c++", "commandline parser")
+    no_copy_source = True
+    exports_sources = ["include/*"]
+
+    def source(self):
+        pkg_name = "v" + self.version + ".zip"
+        tools.download(self.url + "/archive/" + pkg_name, pkg_name)
+        tools.unzip(pkg_name)
+
+    def package(self):
+        src_folder = self.name + "-" + self.version
+        self.copy("*.h", "include", src_folder + "/include")
